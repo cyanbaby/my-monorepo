c78529749061750db7d8c1bedea05e985d661bb3
cyanbaby <525aj@sina.com>
2026-03-22 19:15:58 +0800

    feat: loading & error & not-found

--- nextjs-study/xiao-man-demo-1/src/app/about/page.tsx
+++ nextjs-study/xiao-man-demo-1/src/app/about/page.tsx
@@ -1,3 +1,13 @@
-export default function About() {
+const getData = async () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('data');
+    }, 5000);
+  });
+};
+
+export default async function About() {
+  const data = await getData(); // 演示 loading
+  // const data = await getData2(); // 演示 error
   return <div>About</div>;
 }
