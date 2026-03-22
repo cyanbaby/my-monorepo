72c45340a15e6cf3ba06e62fdc312b994da4900d
cyanbaby <525aj@sina.com>
2026-03-22 22:01:48 +0800

    feat: redirect, permanentRedirect

--- nextjs-study/xiao-man-demo-1/src/app/about/page.tsx
+++ nextjs-study/xiao-man-demo-1/src/app/about/page.tsx
@@ -1,17 +1,15 @@
 'use client';
 
-import { useRouter } from "next/navigation";
+import { redirect, permanentRedirect } from "next/navigation";
 
 export default function About() {
-  const router = useRouter();
+  if(true) {
+    // redirect("/about/me");  // 307
+    permanentRedirect("/about/me");  // 永久重定向
+  }
   return (
     <div className="h-500">
       <h1>About</h1>
-        <button className="block text-blue-500" onClick={() => router.replace("/page")}>替换当前页面</button>
-        <button className="block text-blue-500" onClick={() => router.back()}>返回上一页</button>
-        <button className="block text-blue-500" onClick={() => router.forward()}>跳转下一页</button>
-        <button className="block text-blue-500" onClick={() => router.refresh()}>刷新当前页面</button>
-        <button className="block text-blue-500" onClick={() => router.prefetch("/about")}>预获取about页面</button>
     </div>
   );
 }
