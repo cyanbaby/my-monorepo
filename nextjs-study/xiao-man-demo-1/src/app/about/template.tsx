7d7bba210e5216643d6696820c2f67c9b60a7168
cyanbaby <525aj@sina.com>
2026-03-22 19:10:33 +0800

    feat: feat: 小满课 - App 路由 - layout 和 template 区别 - 数据缓存 - 挂载次数

--- nextjs-study/xiao-man-demo-1/src/app/about/template.tsx
+++ nextjs-study/xiao-man-demo-1/src/app/about/template.tsx
@@ -1,12 +1,35 @@
+'use client';
+import { useState, useEffect } from "react";
+import Link from "next/link";
+
 export default function AboutTemplate({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log('about/template 挂载');
+    return () => {
+      console.log('about/template 卸载');
+    }
+  }, []);
   return (
     <div>
       <header>About Template Header</header>
+      <button onClick={() => setCount(count + 1)}>about/template +1</button>
+      <p>count: {count}</p>
+
+      <hr />
+
       {children}
+
+      <hr />
+
+
+      <Link className="text-blue-500 block" href="/about/me">about/me</Link>
+      <Link className="text-blue-500 block" href="/about/me2">about/me2</Link>
+
       <footer>About Template Footer</footer>
     </div>
   )
